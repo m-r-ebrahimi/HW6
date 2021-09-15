0c9ad3e3e670cc81cbee438c2b974369d1f67bc2
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-15 12:05:56 +0430

    make runner

--- src/com/Maktab/Customer.java
+++ src/com/Maktab/Customer.java
@@ -72,3 +72,43 @@ public class Customer {
         cart[i++] = item;
     }
 }
+
+class Address {
+    private String state;
+    private String city;
+    private String street;
+    private int postalCode;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+}
+
