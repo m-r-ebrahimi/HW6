8b2e1732a97c9862dae822a874e510c7d944a24f
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-15 11:13:13 +0430

    first run

--- src/com/Maktab/Main.java
+++ src/com/Maktab/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Store store = new Store();
+        Customer customer=new Customer();
+        store.makeStore();
     }
 }
--- src/com/Maktab/Store.java
+++ src/com/Maktab/Store.java
@@ -9,5 +9,38 @@ public class Store {
     ArrayList<Magazine> magazines=new ArrayList();
     ArrayList<SportShoe> sportShoes=new ArrayList();
     ArrayList<OxfordShoe> oxfordShoes=new ArrayList();
-
+    public void makeStore(){
+        Radio radio1=new Radio(1,6,1200);
+        Radio radio2=new Radio(2,8,1250);
+        Radio radio3=new Radio(3,3,430);
+        Radio radio4=new Radio(4,20,14000);
+        Radio radio5=new Radio(5,14,140);
+        radios.add(radio1);
+        radios.add(radio2);
+        radios.add(radio3);
+        radios.add(radio4);
+        radios.add(radio5);
+        Television television1=new Television(1,45,8700);
+        Television television2=new Television(2,32,18000);
+        televisions.add(television1);
+        televisions.add(television2);
+        Book book1 = new Book(1,20,80);
+        books.add(book1);
+        Magazine magazine1=new Magazine(1,56,980);
+        Magazine magazine2=new Magazine(2,6,1980);
+        Magazine magazine3=new Magazine(3,42,540);
+        magazines.add(magazine1);
+        magazines.add(magazine2);
+        magazines.add(magazine3);
+        SportShoe sportShoe1=new SportShoe(1,25,98000);
+        SportShoe sportShoe2=new SportShoe(2,25,98500);
+        SportShoe sportShoe3=new SportShoe(3,25,98600);
+        SportShoe sportShoe4=new SportShoe(4,25,98800);
+        sportShoes.add(sportShoe1);
+        sportShoes.add(sportShoe2);
+        sportShoes.add(sportShoe3);
+        sportShoes.add(sportShoe4);
+        OxfordShoe oxfordShoe1=new OxfordShoe(1,30,10000);
+        oxfordShoes.add(oxfordShoe1);
+    }
 }
