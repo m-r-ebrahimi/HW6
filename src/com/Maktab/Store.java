310563b8342ee389cacb323588c84405f350af52
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-15 10:51:18 +0430

    Update Store

---
+++ src/com/Maktab/Store.java
@@ -0,0 +1,12 @@
+package com.Maktab;
+
+import java.util.ArrayList;
+
+public class Store {
+    ArrayList<Radio> radios=new ArrayList();
+    ArrayList<Television> televisions=new ArrayList();
+    ArrayList<Book> books=new ArrayList();
+    ArrayList<Magazine> magazines=new ArrayList();
+    ArrayList<SportShoe> sportShoes=new ArrayList();
+
+}
