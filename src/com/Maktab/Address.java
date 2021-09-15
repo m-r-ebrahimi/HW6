26a481759ff15b28162cc59a5ea8010fcb147478
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-15 10:24:28 +0430

    add address class

---
+++ src/com/Maktab/Address.java
@@ -0,0 +1,40 @@
+package com.Maktab;
+
+public class Address {
+    private String state;
+    private String city;
+    private String street;
+    private int postalCode;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+}
