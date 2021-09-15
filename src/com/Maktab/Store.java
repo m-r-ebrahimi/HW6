3cd122137f50d455e258d296eedb979c392bb8d7
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-15 11:39:07 +0430

    fill store

--- src/com/Maktab/Store.java
+++ src/com/Maktab/Store.java
@@ -1,46 +1,38 @@
 package com.Maktab;
 
-import java.util.ArrayList;
+
 
 public class Store {
-    ArrayList<Radio> radios=new ArrayList();
-    ArrayList<Television> televisions=new ArrayList();
-    ArrayList<Book> books=new ArrayList();
-    ArrayList<Magazine> magazines=new ArrayList();
-    ArrayList<SportShoe> sportShoes=new ArrayList();
-    ArrayList<OxfordShoe> oxfordShoes=new ArrayList();
-    public void makeStore(){
-        Radio radio1=new Radio(1,6,1200);
-        Radio radio2=new Radio(2,8,1250);
-        Radio radio3=new Radio(3,3,430);
-        Radio radio4=new Radio(4,20,14000);
-        Radio radio5=new Radio(5,14,140);
-        radios.add(radio1);
-        radios.add(radio2);
-        radios.add(radio3);
-        radios.add(radio4);
-        radios.add(radio5);
-        Television television1=new Television(1,45,8700);
-        Television television2=new Television(2,32,18000);
-        televisions.add(television1);
-        televisions.add(television2);
-        Book book1 = new Book(1,20,80);
-        books.add(book1);
-        Magazine magazine1=new Magazine(1,56,980);
-        Magazine magazine2=new Magazine(2,6,1980);
-        Magazine magazine3=new Magazine(3,42,540);
-        magazines.add(magazine1);
-        magazines.add(magazine2);
-        magazines.add(magazine3);
-        SportShoe sportShoe1=new SportShoe(1,25,98000);
-        SportShoe sportShoe2=new SportShoe(2,25,98500);
-        SportShoe sportShoe3=new SportShoe(3,25,98600);
-        SportShoe sportShoe4=new SportShoe(4,25,98800);
-        sportShoes.add(sportShoe1);
-        sportShoes.add(sportShoe2);
-        sportShoes.add(sportShoe3);
-        sportShoes.add(sportShoe4);
-        OxfordShoe oxfordShoe1=new OxfordShoe(1,30,10000);
-        oxfordShoes.add(oxfordShoe1);
+    private Radio[] radios = new Radio[5];
+    private Television[] televisions = new Television[2];
+    private Book[] books = new Book[1];
+    private Magazine[] magazines = new Magazine[3];
+    private SportShoe[] sportShoes = new SportShoe[4];
+    private OxfordShoe[] oxfordShoes = new OxfordShoe[1];
+
+    public void makeStore() {
+        radios[0] = new Radio(1, 6, 1200);
+        radios[1] = new Radio(2, 8, 1250);
+        radios[2] = new Radio(3, 3, 430);
+        radios[3] = new Radio(4, 20, 14000);
+        radios[4] = new Radio(5, 14, 140);
+
+        televisions[0] = new Television(1, 45, 8700);
+        televisions[1] = new Television(2, 32, 18000);
+
+        books[0] = new Book(1, 20, 80);
+
+        magazines[0] = new Magazine(1, 56, 980);
+        magazines[1] = new Magazine(2, 6, 1980);
+        magazines[2] = new Magazine(3, 42, 540);
+
+        sportShoes[0] = new SportShoe(1, 25, 98000);
+        sportShoes[1] = new SportShoe(2, 25, 98500);
+        sportShoes[2] = new SportShoe(3, 25, 98600);
+        sportShoes[3] = new SportShoe(4, 25, 98800);
+
+        oxfordShoes[0] = new OxfordShoe(1, 30, 10000);
+
     }
+
 }
