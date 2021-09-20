7cb189091eb19630bd34e546436442582179cc09
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-20 11:04:13 +0430

    Refactor Address Class

---
+++ src/com/Maktab/person/Address.java
@@ -0,0 +1,51 @@
+package com.Maktab.person;
+
+public class Address {
+    private String state;
+    private String city;
+    private String street;
+    private int postalCode;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "state='" + state + '\'' +
+                ", city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", postalCode=" + postalCode +
+                '}';
+    }
+
+}
