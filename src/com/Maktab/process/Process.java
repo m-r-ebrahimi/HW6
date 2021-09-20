19d54245b11b5cb2ba0cff96148fdab55e24c52f
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-20 11:24:36 +0430

    Refactor Process Class, Add makeCustomer method

--- src/com/Maktab/process/Process.java
+++ src/com/Maktab/process/Process.java
@@ -1,4 +1,46 @@
 package com.Maktab.process;
 
+import com.Maktab.person.Address;
+import com.Maktab.person.Customer;
+
+import java.util.Scanner;
+
 public class Process {
+    Scanner scanner = new Scanner(System.in);
+    public void run(){
+
+    }
+    public void makeCustomer(Customer customer) {
+        Address address ;
+        System.out.println("Enter user_name:");
+        String username = scanner.nextLine();
+        System.out.println("Enter password:");
+        String password = scanner.nextLine();
+        System.out.println("Enter firstname:");
+        String firstname = scanner.nextLine();
+        System.out.println("Enter lastname:");
+        String lastname = scanner.nextLine();
+        System.out.println("Enter phone number:");
+        String phone = scanner.nextLine();
+        System.out.println("Enter email:");
+        String email = scanner.nextLine();
+        System.out.println("Enter state:");
+        String state = scanner.nextLine();
+        System.out.println("Enter city:");
+        String city = scanner.nextLine();
+        System.out.println("Enter street:");
+        String street = scanner.nextLine();
+        System.out.println("Enter postal code:");
+        int code = scanner.nextInt();
+        scanner.nextLine();
+        customer.setUsername(username);
+        customer.setPassword(password);
+        customer.setFirstName(firstname);
+        customer.setLastName(lastname);
+        customer.setPhoneNumber(phone);
+        customer.setEmail(email);
+        address=new Address(state,city,street,code);
+        customer.setAddress(address);
+    }
+
 }
