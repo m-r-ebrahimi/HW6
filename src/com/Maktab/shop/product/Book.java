dc78b90a14508ef5911f502096ed11a8b7b81b8b
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-20 11:40:55 +0430

    Refactor Book Class

--- src/com/Maktab/shop/product/Book.java
+++ src/com/Maktab/shop/product/Book.java
@@ -3,6 +3,9 @@ package com.Maktab.shop.product;
 public class Book extends Product {
     private String publisherName;
 
+    public Book() {
+    }
+
     public Book(int id, String name, int price, int count, String publisher) {
         super(id, name, price, count);
         publisherName = publisher;
@@ -20,6 +23,6 @@ public class Book extends Product {
     public String toString() {
         return "Book{" + super.toString() +
                 "publisherName='" + publisherName + '\'' +
-                '}'+"\n";
+                '}' + "\n";
     }
 }
