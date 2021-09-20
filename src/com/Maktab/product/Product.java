d982880b3052eff204785b0c945a887c8c2c7884
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-20 10:31:19 +0430

    Refactor Product Class

--- src/com/Maktab/product/Product.java
+++ src/com/Maktab/product/Product.java
@@ -1,14 +1,16 @@
 package com.Maktab.product;
 
-public abstract class Product {
+public class Product {
     private int id;
     private String name;
     private int price;
+    private int count;
 
-    public Product(int id, String name, int price) {
+    public Product(int id, String name, int price, int count) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.count = count;
     }
 
     public int getId() {
@@ -27,14 +29,37 @@ public abstract class Product {
         this.name = name;
     }
 
-    abstract void add(int num);
+    public int getPrice() {
+        return price;
+    }
 
-    abstract void remove(int num);
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    void add(int num) {
+        count += num;
+    }
+
+    void remove(int num) {
+        if (count - num >= 0)
+            count -= num;
+        else System.out.println("Error!!");
+    }
 
     @Override
     public String toString() {
         return "id=" + id +
                 ", name='" + name + '\'' +
-                ", price=" + price;
+                ", price=" + price +
+                ", count=" + count;
     }
 }
